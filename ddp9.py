c370d7a1f3d61285832d126868eeb9f2dc0c8563
Akidnaa <146068932+Akidnaa@users.noreply.github.com>
2023-12-04 22:28:51 +0700

    tugas ddp pertemuan ke 9

---
+++ ddp9.py
@@ -0,0 +1,14 @@
+nama_siswa_yang_lulus = ["Jefri","Udin","Eva","Laila"]
+
+buah = "Banana"
+nama_buah_terbalik = buah [::-1]
+
+nama_buah_duplikat = buah *2
+
+kata = "Vengeance"
+konsonan = ''.join(char for char in kata if char.lower() not in 'aiueo')
+
+print("#1.nama siswa yang lulus :", nama_siswa_yang_lulus)
+print("#2.nama dibalik :", nama_buah_terbalik)
+print("#3.nama duplikat :",nama_buah_duplikat)
+print("#4. Konsonan kata :", konsonan)
